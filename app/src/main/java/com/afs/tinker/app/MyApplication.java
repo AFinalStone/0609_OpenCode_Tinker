a2153c43b968f1156bbb4b7ad8605c99285bcc83
shiyaolei <shiyaolei@yongrun.com>
2022-05-11 18:38:01 +0800

    添加腾讯热修复依赖

---
+++ app/src/main/java/com/afs/tinker/app/MyApplication.java
@@ -0,0 +1,13 @@
+//package com.afs.tinker.app;
+//
+//import com.tencent.tinker.loader.TinkerLoader;
+//import com.tencent.tinker.loader.app.TinkerApplication;
+//import com.tencent.tinker.loader.shareutil.ShareConstants;
+//
+//public class MyApplication extends TinkerApplication {
+//
+//    public MyApplication() {
+//        super(ShareConstants.TINKER_ENABLE_ALL, TinkerApplicationLike.class.getName(), TinkerLoader.class.getName(), false);
+//    }
+//
+//}
